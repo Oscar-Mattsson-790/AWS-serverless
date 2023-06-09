35544ec63de0e03e6a9e69361f1c6a9facc696db
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-06-09 08:28:29 +0200

    done

---
+++ functions/postInsults.js
@@ -0,0 +1,17 @@
+const { sendResponse } = require("../responses/index");
+
+function checkBody(body) {
+  if (body?.insult && body?.play) return true;
+  else return false;
+}
+
+function postInsult(insults, body) {
+  if (checkBody(body)) {
+    insults.push(body);
+
+    return sendResponse(200, { success: true });
+  } else 8;
+  return sendResponse(400, { message: "Wrong data in body" });
+}
+
+module.exports = { postInsult };
